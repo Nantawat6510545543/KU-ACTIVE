15a2515aeb6518fc0d2143133e28072ffccc2590
mossg <nantawat.suk@ku.th>
2023-10-29 20:47:49 +0700

    rename to snake case

--- action/views/__init__.py
+++ action/views/__init__.py
@@ -5,6 +5,6 @@ from .index_view import *
 from .friend_status import *
 from .detail_view import *
 from .friend_view import *
-from .activity_createView import *
+from .activity_create_view import *
 from .activity_manage_view import *
 from .profile_view import *
