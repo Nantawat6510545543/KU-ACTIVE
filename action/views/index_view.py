c8bddbab3b2490b60d16576249fba4e9298e62ed
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2023-10-30 14:41:54 +0700

    implement sessions for search/sort (will cleanup later)

--- action/views/index_view.py
+++ action/views/index_view.py
@@ -2,6 +2,13 @@ from django.views import generic
 
 from .. import utils
 
+TAG_OPTIONS = [
+    ('title', 'Title'),
+    ('owner', 'Owner'),
+    ('tag', 'Tag'),
+    ('place', 'Place'),
+    ('date', 'Date'),
+]
 
 class IndexView(generic.ListView):
     template_name = 'action/index.html'
@@ -9,3 +16,11 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return utils.get_index_queryset(self.request)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Add the tag options to the context
+        context['tags'] = TAG_OPTIONS
+        utils.update_sessions(self.request)
+        return context
