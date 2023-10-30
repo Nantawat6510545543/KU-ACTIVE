33308ff29a013dfc930858af1f67f92252574f10
mossg <nantawat.suk@ku.th>
2023-10-30 07:34:07 +0700

    use try except instant of if else

--- action/utils/encoder.py
+++ action/utils/encoder.py
@@ -6,7 +6,7 @@ from io import BytesIO
 def image_to_base64(image_file):
     max_size = 1024
 
-    if image_file is not None:
+    try:
         img = Image.open(image_file)
         img = img.resize((max_size, max_size))
         img = img.convert('RGB')
@@ -17,4 +17,5 @@ def image_to_base64(image_file):
 
         base64_encoded = base64.b64encode(image_data).decode('utf-8')
         return base64_encoded
-    return ""
+    except AttributeError:
+        return ""
