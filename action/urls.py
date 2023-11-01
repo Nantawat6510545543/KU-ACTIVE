0f1c7e40fb236b0343d6f8c3ed30c95738943e81
mossg <nantawat.suk@ku.th>
2023-11-01 12:12:08 +0700

    update url

--- action/urls.py
+++ action/urls.py
@@ -22,14 +22,14 @@ urlpatterns = [
 
     path('manage/', views.ActivityManageView.as_view(), name='manage'),
     path('create/', views.ActivityCreateView.as_view(), name='create'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:activity_id>/', views.DetailView.as_view(), name='detail'),
     path('edit/<int:activity_id>', views.ActivityEditView.as_view(), name='edit'),
     path('delete/<int:activity_id>', views.delete_activity, name='delete_activity'),
 
-    path('<int:activity_id>/participate/', views.participate, name='participate'),
-    path('<int:activity_id>/leave/', views.leave, name='leave'),
-    path('<int:activity_id>/favorite/', views.favorite, name='favorite'),
-    path('<int:activity_id>/unfavorite/', views.unfavorite, name='unfavorite'),
+    path('participate/<int:activity_id>', views.participate, name='participate'),
+    path('leave/<int:activity_id>', views.leave, name='leave'),
+    path('favorite/<int:activity_id>', views.favorite, name='favorite'),
+    path('unfavorite/<int:activity_id>', views.unfavorite, name='unfavorite'),
 
     path('calendar/', views.CalendarView.as_view(), name='calendar')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
