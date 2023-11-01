b430c4efbe41e3a0d28e21c0c6f2eec3316413eb
mossg <nantawat.suk@ku.th>
2023-11-01 14:46:50 +0700

    Update the database settings in case they are not found.

--- mysite/db.py
+++ mysite/db.py
@@ -5,19 +5,24 @@ import dj_database_url
 from decouple import config
 from mysite.settings import BASE_DIR
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 try:
     DATABASE_URL = config("DATABASE_URL", default=None)
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True
-        )
-    }
-except ValueError:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+    if DATABASE_URL not in [None, '']:
+        DATABASES = {
+            'default': dj_database_url.config(
+                default=DATABASE_URL,
+                conn_max_age=600,
+                conn_health_checks=True
+            )
         }
-    }
+    else:
+        print("DATABASE_URL not found. Auto-migrating on local database.")
+except ValueError:
+    print("Invalid DATABASE_URL. Auto-migrating on local database.")
