b60ff407c15dd1e93326dc5333687a489c2e1696
mossg <nantawat.suk@ku.th>
2023-11-05 23:22:38 +0700

    update friend status docstring and test

--- action/tests/tests_models/test_friend_status_model.py
+++ action/tests/tests_models/test_friend_status_model.py
@@ -4,15 +4,35 @@ from action.tests import utils
 
 class FriendStatusTestCase(TestCase):
     def setUp(self):
+        super().setUp()
         self.sender = utils.create_user(username="sender")
-        self.receiver = utils.create_user(username="receiver")
-        self.friend_status = utils.create_friend_status(sender=self.sender,
-                                                        receiver=self.receiver)
+        self.receiver_list = [utils.create_user(f'receiver{i}') for i in
+                              range(1, 5)]
+
+        receiver_statuses = {
+            self.receiver_list[0]: None,
+            self.receiver_list[1]: 'Unknown',
+            self.receiver_list[2]: 'Pending',
+            self.receiver_list[3]: 'Accepted'
+        }
+
+        self.friend_status_list = [
+            utils.create_friend_status(sender=self.sender, receiver=receiver,
+                                       request_status=status)
+            for receiver, status in receiver_statuses.items()
+        ]
 
     def test_friend_status_attributes(self):
-        friend_status = self.friend_status
+        statuses_check = {
+            self.receiver_list[0]: (None, False),
+            self.receiver_list[1]: (None, False),
+            self.receiver_list[2]: ('Pending', False),
+            self.receiver_list[3]: ('Accepted', True)
+        }
 
-        self.assertEqual(friend_status.sender, self.sender)
-        self.assertEqual(friend_status.receiver, self.receiver)
-        self.assertIsNone(friend_status.request_status)
-        self.assertFalse(friend_status.is_friend)
+        for status, (expected_request_status, expected_is_friend) in zip(
+                self.friend_status_list, statuses_check.values()):
+            self.assertEqual(status.sender, self.sender)
+            self.assertIn(status.receiver, self.receiver_list)
+            self.assertEqual(status.request_status, expected_request_status)
+            self.assertEqual(status.is_friend, expected_is_friend)
--- action/tests/utils.py
+++ action/tests/utils.py
@@ -30,13 +30,16 @@ def create_user(username='tester', password='password', **kwargs):
     user = User.objects.create_user(username=username, password=password)
 
     user_fields = {
+        "event_encoder": {},
         "email": None,
         "first_name": None,
         "last_name": None,
-        "active": None,
+        "active": None
     }
 
     user_fields.update(kwargs)
+
+    user.email = user_fields["event_encoder"]
 
     if user_fields["email"] is not None:
         user.email = user_fields["email"]
@@ -50,7 +53,6 @@ def create_user(username='tester', password='password', **kwargs):
     if user_fields["active"] is not None:
         user.active = user_fields["active"]
 
-    user.save()
     return user
 
 
@@ -101,8 +103,6 @@ def create_activity(owner, **kwargs):
     if defaults["tags"] is not None:
         activity.tags.set(defaults["tags"])
 
-    activity.save()
-
     return activity
 
 
@@ -117,20 +117,38 @@ def create_activity_status(participants, activity, participated=True,
     if favorite is not None:
         activity_status.is_favorited = favorite
 
-    activity_status.save()
-
     return activity_status
 
 
-def create_friend_status(sender, receiver, choices=None, is_friend=None):
+def create_friend_status(sender: User, receiver: User,
+                         request_status: str = None) -> FriendStatus:
+    """
+    Create a friend status record in the database.
+
+    Args:
+        sender (User): The User who initiated the friendship request.
+        receiver (User): The User who received the friendship request.
+        request_status (str, optional): The request status, which can be one of 'Pending',
+            'Accepted', or 'Declined'. Defaults to None if not specified or if an invalid
+            status is provided.
+
+    Returns:
+        FriendStatus: The created FriendStatus object representing the friendship request.
+
+    Note:
+        - The created FriendStatus object will have an 'is_friend' field set to True if the
+          request_status is 'Accepted', indicating that the users are now friends.
+    """
+    if (request_status is not None and
+            request_status.title() not in ['Pending', 'Accepted', 'Declined']):
+        request_status = None
+
     friend_status = FriendStatus.objects.create(
         sender=sender,
         receiver=receiver,
     )
-    if choices is not None:
-        friend_status.choices = choices
-    if is_friend is not None:
-        friend_status.is_friend = is_friend
+    if request_status is not None:
+        friend_status.request_status = request_status
+        friend_status.is_friend = request_status == 'Accepted'
 
-    friend_status.save()
     return friend_status
