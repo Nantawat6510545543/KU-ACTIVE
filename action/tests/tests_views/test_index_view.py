b8f7cc1b7828058298101bfdc03ae3f0bce90374
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2023-11-11 15:39:24 +0700

    Remove unuse imports

--- action/tests/tests_views/test_index_view.py
+++ action/tests/tests_views/test_index_view.py
@@ -1,15 +1,7 @@
-from datetime import timedelta
-
-from django.urls import reverse
-from django.contrib.auth.models import User
-from django.test import TestCase
 from django.utils import timezone
-
-from mysite import settings
-from action.models import *
-from action.tests import utils
-from action.tests.utils import *
-from action.forms.activity_form import *
+from django.urls import reverse
+from django.test import TestCase
+from action.tests.utils import create_activity, create_user
 
 
 class ActivityIndexViewTests(TestCase):
