84072b876646ce0eb922a9bf519716ba0e26a487
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2023-11-12 21:23:56 +0700

    Write tests on activity manage view

--- action/tests/tests_views/test_activity_view/test_manage_view.py
+++ action/tests/tests_views/test_activity_view/test_manage_view.py
@@ -12,17 +12,60 @@ from action.tests.utils import *
 from action.forms.activity_form import *
 
 
+# TODO :
 class ActivityManageViewTests(TestCase):
     def setUp(self) -> None:
-        self.user = create_user()
+        user_data = {"email": "test@example.com"}
+        self.user = create_user(username="John", password="abc", **user_data)
+        self.activity = create_activity(self.user)
 
-    def test_manage_activity_redirect(self):
+    def test_manage_activity_guest(self):
+        """
+        Guest should not have a permission to access this page.
+        Should be redirected.
+        """
         url = reverse('action:manage')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
 
-    def test_delete_activity_redirect(self):
-        available_activity = create_activity(self.user)
-        url = reverse('action:delete_activity', args=(available_activity.id,))
+    def test_manage_activity_authenticated(self):
+        """
+        Authenticated users should have a permission to access this page.
+        """
+        self.client.force_login(self.user)
+        url = reverse('action:manage')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_guest_delete_activity(self):
+        """
+        Guest should not have permission to access this feature.
+        Should be redirected.
+        """
+        url = reverse('action:delete_activity', args=(self.activity.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
+
+    # TODO: Redirect can't use with this feature
+    # def test_delete_others_activity(self):
+    #     """
+    #     Any authenticated user should not be able to delete others activities.
+    #     Should be redirected.
+    #     """
+    #     user_data = {"email": "test2@example.com"}
+    #     other = create_user(username="Tim", password="abc", **user_data)
+    #     self.client.force_login(other)
+    #     url = reverse('action:delete_activity', args=(self.activity.id,))
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, 302)
+    #
+    # def test_delete_self_activity(self):
+    #     """
+    #     Authenticated users can delete their own activity.
+    #     """
+    #     self.client.force_login(self.user)
+    #     url = reverse('action:delete_activity', args=(self.activity.id,))
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, 302)
+    #     # TODO: Check message instead
+    #     self.assertIs(response.context['messages'], "a")
