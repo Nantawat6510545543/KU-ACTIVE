6e46f9873a17912853b709d67790aa85ccfa9a8a
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2023-11-12 23:11:25 +0700

    Write tests on profile edit

--- action/tests/tests_views/test_profile_view/test_profile_edit_view.py
+++ action/tests/tests_views/test_profile_view/test_profile_edit_view.py
@@ -1,10 +1,27 @@
-import datetime
-
-import django.test
 from django.urls import reverse
-from django.contrib.auth.models import User
 from django.test import TestCase
-from django.utils import timezone
-from action.models import *
-from action.tests import utils
-from mysite import settings
+from action.tests.utils import create_user
+
+
+class ProfileEditViewTests(TestCase):
+    def setUp(self) -> None:
+        user_data_1 = {"email": "test1@example.com"}
+        self.user_1 = create_user(username="John", password="abc", **user_data_1)
+        # user_data_2 = {"email": "test2@example.com"}
+        # self.user_2 = create_user(username="Jane", password="abc", **user_data_2)
+
+    def test_guest_view_profile_edit(self):
+        """
+        Guest should not be able to edit their own profile.
+        Should be redirected.
+        """
+        response = self.client.get(reverse('action:profile'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_authenticated_view_profile_edit(self):
+        """
+        Authenticated users should be able to access this page.
+        """
+        self.client.force_login(self.user_1)
+        response = self.client.get(reverse('action:profile'))
+        self.assertEqual(response.status_code, 200)
