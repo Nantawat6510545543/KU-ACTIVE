eb4d45aff40281b64320b8dd5018029ad9a485a2
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2023-11-12 21:30:03 +0700

    Delete un use imports

--- action/tests/tests_views/test_activity_view/test_manage_view.py
+++ action/tests/tests_views/test_activity_view/test_manage_view.py
@@ -1,18 +1,8 @@
-from datetime import timedelta
-
 from django.urls import reverse
-from django.contrib.auth.models import User
 from django.test import TestCase
-from django.utils import timezone
-
-from mysite import settings
-from action.models import *
-from action.tests import utils
-from action.tests.utils import *
-from action.forms.activity_form import *
+from action.tests.utils import create_activity, create_user
 
 
-# TODO :
 class ActivityManageViewTests(TestCase):
     def setUp(self) -> None:
         user_data = {"email": "test@example.com"}
