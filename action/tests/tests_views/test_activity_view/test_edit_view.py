a16ff2d6dd53059e7d58337fe21a4ea8c3fcb0e4
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2023-11-12 09:11:23 +0700

    Add tests on edit view

--- action/tests/tests_views/test_activity_view/test_edit_view.py
+++ action/tests/tests_views/test_activity_view/test_edit_view.py
@@ -1,23 +1,40 @@
-from datetime import timedelta
-
 from django.urls import reverse
-from django.contrib.auth.models import User
 from django.test import TestCase
-from django.utils import timezone
-
-from mysite import settings
-from action.models import *
-from action.tests import utils
-from action.tests.utils import *
-from action.forms.activity_form import *
+from action.tests.utils import create_activity, create_user
 
 
 class ActivityEditViewTests(TestCase):
     def setUp(self) -> None:
-        self.user = create_user()
+        user_data = {"email": "test@example.com"}
+        self.user = create_user(username="John", password="abc", **user_data)
+        self.activity = create_activity(self.user)
 
-    def test_activity_edit(self):
-        available_activity = create_activity(self.user)
-        url = reverse('action:edit', args=(available_activity.id,))
+    def test_access_activity_edit_guest(self):
+        """
+        Unauthenticated users are not allowed to access this function.
+        Should be redirected to the index page.
+        """
+        url = reverse('action:edit', args=(self.activity.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
+
+    def test_access_activity_edit_authenticated(self):
+        """
+        Authenticated users are allowed to edit their own activity.
+        """
+        self.client.force_login(self.user)
+        url = reverse('action:edit', args=(self.activity.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    # TODO: BUGS HERE
+    # def test_others_edit_activity(self):
+    #     """
+    #     Nobody is allowed to edit others activities.
+    #     """
+    #     user_data = {"email": "test2@example.com"}
+    #     another_user = create_user(username="Jane", password="abc", **user_data)
+    #     self.client.force_login(another_user)
+    #     url = reverse('action:edit', args=(self.activity.id,))
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, 302)
