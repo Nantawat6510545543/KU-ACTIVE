5f7bcd70d5227975d8500b999be68546f6e3e428
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2023-11-12 13:27:07 +0700

    adjust code + fix previous bug

--- action/utils/encoder.py
+++ action/utils/encoder.py
@@ -2,23 +2,20 @@ import base64
 from PIL import Image, UnidentifiedImageError
 from io import BytesIO
 
-MAX_SIZE = (1024, 1024)
-CONVERT_TYPE = 'RGB'
-FORMAT = 'JPEG'
-UNICODE = 'utf-8'
-
 
 def image_to_base64(image_file):
+    max_size = 1024
+
     try:
         img = Image.open(image_file)
-        img = img.resize(MAX_SIZE)
-        img = img.convert(CONVERT_TYPE)
+        img = img.resize((max_size, max_size))
+        img = img.convert('RGB')
 
         img_byte_array = BytesIO()
-        img.save(img_byte_array, format=FORMAT)
+        img.save(img_byte_array, format='JPEG')
         image_data = img_byte_array.getvalue()
 
-        base64_encoded = base64.b64encode(image_data).decode(UNICODE)
+        base64_encoded = base64.b64encode(image_data).decode('utf-8')
         return base64_encoded
     except (ValueError, AttributeError, UnidentifiedImageError):
         return ""
