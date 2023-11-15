1aa08c48d3a30cb2be7a015da8970b5f2909e016
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2023-11-15 17:20:16 +0700

    Edit calendar view

--- action/views/calendar_view.py
+++ action/views/calendar_view.py
@@ -1,11 +1,4 @@
 from django.views import generic
 
-from action.utils import search_utils
-
-
-class CalendarView(generic.ListView):
+class CalendarView(generic.TemplateView):
     template_name = 'action/calendar.html'
-    context_object_name = 'activity_list'
-
-    def get_queryset(self):
-        return search_utils.get_index_queryset(self.request)
