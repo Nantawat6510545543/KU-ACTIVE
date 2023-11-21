4a349992c2f69d17f0d5e4f556c13766d639fac7
mossg <nantawat.suk@ku.th>
2023-11-21 13:38:49 +0700

    update gunicorn-setup

--- gunicorn-setup.sh
+++ gunicorn-setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Run Gunicorn
-gunicorn --bind :8000 --workers 2 mysite.wsgi
-
 # Swap setup
 fallocate -l 512M /swapfile
 chmod 0600 /swapfile
@@ -10,3 +7,6 @@ mkswap /swapfile
 echo 10 > /proc/sys/vm/swappiness
 swapon /swapfile
 echo 1 > /proc/sys/vm/overcommit_memory
+
+# Run Gunicorn
+gunicorn --bind :8000 --workers 2 mysite.wsgi
