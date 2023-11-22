912158b5a04abac48216665ee1fcb696c2ab5157
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2023-11-22 17:09:21 +0700

    make other people's profile viewable as guest

--- action/views/profile/detail_view.py
+++ action/views/profile/detail_view.py
@@ -1,5 +1,4 @@
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.views import generic
@@ -7,7 +6,7 @@ from django.views import generic
 from action.models import User
 
 
-class ProfileView(LoginRequiredMixin, generic.ListView):
+class ProfileView(generic.ListView):
     template_name = 'action/profile/detail.html'
     context_object_name = 'profile'
 
@@ -15,11 +14,16 @@ class ProfileView(LoginRequiredMixin, generic.ListView):
         if user_id is None:  # Set default value to current user
             user_id = request.user.id
 
-        try:
+        try:  # Loading a profile view
             profile = User.objects.get(id=user_id)
             context = {self.context_object_name: profile}
             return render(request, self.template_name, context)
 
         except User.DoesNotExist:
+            # Case where user try to view their profile but not logged in
+            if not request.user.is_authenticated:
+                return redirect('login')
+
+            # Case where there's no user profile with that id
             messages.warning(request, "Invalid user id.")
             return redirect('action:index')
