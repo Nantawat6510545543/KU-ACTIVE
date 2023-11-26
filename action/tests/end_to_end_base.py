e08a69a9e1713f84c18ce9ccb6ca282e9b39c2f2
mossg <nantawat.suk@ku.th>
2023-11-26 17:43:45 +0700

    update end_to_end_base docstring

--- action/tests/end_to_end_base.py
+++ action/tests/end_to_end_base.py
@@ -10,42 +10,85 @@ from action.tests.utils import create_social_app
 
 
 class EndToEndTestBase(StaticLiveServerTestCase):
-    """End-to-end Base class"""
+    """Base class for end-to-end tests using Selenium and Firefox."""
 
     browser = None
 
     @classmethod
     def setUpClass(cls):
+        """
+        Set up the Firefox browser for headless mode.
+
+        1. Create Firefox options with the "--headless" argument.
+        2. Initialize the Firefox browser with the created options.
+        """
         super().setUpClass()
         options = FirefoxOptions()
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         cls.browser = Firefox(options=options)
         cls.browser.implicitly_wait(30)
         cls.browser.set_page_load_timeout(30)
 
     @classmethod
     def tearDownClass(cls):
+        """Close the Firefox browser."""
         cls.browser.quit()
         super().tearDownClass()
 
     def setUp(self):
+        """Set up a social app using `create_social_app`."""
         create_social_app()
 
     def open(self, view, web_id):
+        """
+        Open a specific view with optional arguments.
+
+        1. Get the URL for the specified view and arguments.
+        2. Navigate to the URL using the Firefox browser.
+        """
         self.url = self.getUrl(view, (web_id,))
         self.browser.get(self.url)
 
     def find_by_class(self, class_name, element=None):
+        """
+        Find an element by its class name.
+
+        Args:
+            - `class_name`: The class name of the element to find.
+            - `element`: Optional parent element to search within.
+
+        Returns:
+            - The found element.
+        """
         if element is not None:
             return element.find_element(By.CLASS_NAME, class_name)
         return self.browser.find_element(By.CLASS_NAME, class_name)
 
     def getUrl(self, view, args=None):
+        """
+        Get the URL for a given view and arguments.
+
+        Args:
+            - `view`: The view name.
+            - `args`: Optional arguments for the view.
+
+        Returns:
+            - The constructed URL.
+        """
         if args is None:
             return self.live_server_url + reverse(view)
         return self.live_server_url + reverse(view, args=args)
 
     def login(self, username, password):
+        """
+        Perform login with the provided username and password.
+
+        1. Navigate to the login page.
+        2. Find the username and password fields.
+        3. Enter the provided username and password.
+        4. Press the RETURN key.
+        5. Sleep for 1 second to allow the login process to complete.
+        """
         self.browser.get(self.getUrl("login"))
 
         user_field = self.browser.find_element(By.NAME, "username")
