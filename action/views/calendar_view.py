f14239b0dc22ec4f5eae644bfc87a9c9d6a64686
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2023-11-26 16:56:33 +0700

    calendar view is now only for google login user

--- action/views/calendar_view.py
+++ action/views/calendar_view.py
@@ -1,23 +1,26 @@
 from allauth.socialaccount.models import SocialAccount
 
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.views import generic
+
 
 class CalendarView(LoginRequiredMixin, generic.TemplateView):
     """View for displaying a calendar of activities."""
 
     template_name = 'action/calendar.html'
 
-    # def get(self, *args, **kwargs):
-    #     try:
-    #         # Check if the user has a Google social account
-    #         SocialAccount.objects.get(user=self.request.user, provider='google')
-    #     except SocialAccount.DoesNotExist:
-    #         # If the user doesn't have a Google social account, redirect to the Google login URL
-    #         return redirect('socialaccount_signup', 'google')
+    def get(self, *args, **kwargs):
+        try:
+            # Check if the user has a Google social account
+            SocialAccount.objects.get(user=self.request.user, provider='google')
+        except SocialAccount.DoesNotExist:
+            # If the user doesn't have a Google social account, Redirect to the Google login page
+            messages.warning(self.request, "Please login to Google to use the calendar feature.")
+            return redirect(self.request.META.get('HTTP_REFERER')) # Redirect to the page before
 
-    #     # If the user already has a Google social account, proceed with the regular view logic
-    #     return super(CalendarView, self).get(*args, **kwargs)
+            # return render(self.request, 'socialaccount/login.html', {'provider': 'google'})
 
-
+        # If the user already has a Google social account, proceed with the regular view logic
+        return super(CalendarView, self).get(*args, **kwargs)
