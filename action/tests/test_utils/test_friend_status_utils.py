471fdb9bf6257ec49436db0562db3c2ea74cb129
mossg <nantawat.suk@ku.th>
2023-11-26 16:59:38 +0700

    update test_utils docstring

--- action/tests/test_utils/test_friend_status_utils.py
+++ action/tests/test_utils/test_friend_status_utils.py
@@ -1,34 +1,57 @@
 from django.test import TestCase
 from action.models import FriendStatus
 from action.utils import fetch_friend_status
-
-from action.tests.utils import create_user, create_friend_status, \
-    create_request
+from action.tests.utils import create_user, create_friend_status, create_request
 
 
 class FriendStatusUtilsTestCase(TestCase):
+    """Test case for the FriendStatus utility functions."""
+
     def setUp(self):
+        """
+        Set up common attributes for the test methods.
+
+        1. Set the view attribute to 'action:add_friend'.
+        2. Create two user instances, user1 and user2.
+        """
         self.view = 'action:add_friend'
         self.user1 = create_user(username='user1', password='password1')
         self.user2 = create_user(username='user2', password='password2')
 
     def test_user1_sender(self):
-        request = create_request(self.view, [self.user2.id], user=self.user1)
-        friend_status = create_friend_status(self.user1, self.user2,
-                                             request_status='Accepted')
+        """
+        Test fetching FriendStatus when user1 is the sender.
 
+        1. Create a request instance where user1 is the sender.
+        2. Create a FriendStatus instance with request_status 'Accepted'.
+        3. Call fetch_friend_status and assert the result is the created FriendStatus.
+        """
+        request = create_request(self.view, [self.user2.id], user=self.user1)
+        friend_status = create_friend_status(self.user1, self.user2, request_status='Accepted')
         get_friend_status = fetch_friend_status(request, self.user2.id)
         self.assertEqual(get_friend_status, friend_status)
 
     def test_user1_receiver(self):
-        request = create_request(self.view, [self.user1.id], user=self.user2)
-        friend_status = create_friend_status(self.user2, self.user1,
-                                             request_status='Accepted')
+        """
+        Test fetching FriendStatus when user1 is the receiver.
 
+        1. Create a request instance where user1 is the receiver.
+        2. Create a FriendStatus instance with request_status 'Accepted'.
+        3. Call fetch_friend_status and assert the result is the created FriendStatus.
+        """
+        request = create_request(self.view, [self.user1.id], user=self.user2)
+        friend_status = create_friend_status(self.user2, self.user1, request_status='Accepted')
         get_friend_status = fetch_friend_status(request, self.user1.id)
         self.assertEqual(get_friend_status, friend_status)
 
     def test_fetch_friend_status_not_existing(self):
+        """
+        Test fetching FriendStatus for a non-existing relationship.
+
+        1. Create a request instance where user1 is the sender.
+        2. Call fetch_friend_status for user2's id.
+        3. Assert that the result is an instance of FriendStatus.
+        """
         request = create_request(self.view, [self.user2.id], user=self.user1)
         get_friend_status = fetch_friend_status(request, self.user2.id)
         self.assertTrue(isinstance(get_friend_status, FriendStatus))
--- action/tests/test_utils/test_search_utils.py
+++ action/tests/test_utils/test_search_utils.py
@@ -5,16 +5,34 @@ from django.utils import timezone
 from django.test import RequestFactory, TestCase
 from action.utils.search_utils import BaseSearcher
 from action.tests.utils import create_activity, create_activity_status, \
-    create_friend_status ,create_category, create_user, quick_join
+    create_friend_status, create_category, create_user, quick_join
 
 
 class SearchUtilsTests(TestCase):
+    """Test case for the search utility functions."""
+
     def setUp(self) -> None:
-        logging.disable(logging.CRITICAL)  # Disable error message during unittest
+        """
+        Set up common attributes for the test methods.
+
+        1. Disable critical logging messages during unittest.
+        2. Create a RequestFactory instance.
+        3. Create a user instance.
+        """
+        logging.disable(logging.CRITICAL)  # Disable error messages during unittest
         self.factory = RequestFactory()
         self.user = create_user()
 
     def test_selection(self):
+        """
+        Test the selection of search parameters.
+
+        1. Make a GET request with parameters 'tag' as 'title' and 'q' as 'test'.
+        2. Assert that the generated query string is 'tag=title&q=test'.
+        3. Make a GET request with 'tag' as 'place' and 'q' as an empty string.
+        4. Assert that the generated query string is 'tag=place&q='.
+        5. Make an invalid tag case request and assert that it redirects to the index page.
+        """
         response = self.client.get(reverse('action:index'), {'tag': 'title', 'q': 'test'})
         params = response.request['QUERY_STRING']
         self.assertEqual(params, 'tag=title&q=test')
@@ -29,15 +47,34 @@ class SearchUtilsTests(TestCase):
         self.assertRedirects(response, reverse('action:index'))
 
     def test_title_search(self):
+        """
+        Test searching by title.
+
+        1. Create a request with 'tag' as 'title' and 'q' as 'ABC'.
+        2. Set the user attribute for the request.
+        3. Create an activity with the title 'ABC'.
+        4. Create a BaseSearcher instance for the request.
+        5. Get the first query result and assert it is the created activity.
+        """
         request = self.factory.get(reverse('action:index'), {'tag': 'title', 'q': 'ABC'})
-        request.user = self.user  # Do not delete any kind of this line, it will break the code
-        activityABC = create_activity(owner=request.user, title="ABC")
+        request.user = self.user
+        activity_ABC = create_activity(owner=request.user, title="ABC")
 
         searcher = BaseSearcher(request)
         query = searcher.get_index_query().first()
-        self.assertEqual(query, activityABC)
+        self.assertEqual(query, activity_ABC)
 
     def test_owner_search(self):
+        """
+        Test searching by owner.
+
+        1. Create a request with 'tag' as 'owner' and 'q' as 'Tester100'.
+        2. Create a user with the username 'Tester100'.
+        3. Set the user attribute for the request.
+        4. Create an activity with the owner set to the created user.
+        5. Create a BaseSearcher instance for the request.
+        6. Get the first query result and assert it is the created activity.
+        """
         request = self.factory.get(reverse('action:index'), {'tag': 'owner', 'q': 'Tester100'})
         request.user = create_user(username="Tester100")
         activity_tester100 = create_activity(owner=request.user, title="Test Owner")
@@ -47,16 +84,37 @@ class SearchUtilsTests(TestCase):
         self.assertEqual(query, activity_tester100)
 
     def test_categories_search(self):
-        request = self.factory.get(reverse('action:index'), {'tag': 'categories', 'q': 'Electronics'})
+        """
+        Test searching by categories.
+
+        1. Create a request with 'tag' as 'categories' and 'q' as 'Electronics'.
+        2. Set the user attribute for the request.
+        3. Create a category with the name 'electronics'.
+        4. Create an activity with the category set to the created category.
+        5. Create a BaseSearcher instance for the request.
+        6. Get the first query result and assert it is the created activity.
+        """
+        request = self.factory.get(reverse('action:index'),
+                                   {'tag': 'categories', 'q': 'Electronics'})
         request.user = self.user
         activity_categories = create_category(name='electronics')
-        activity = create_activity(owner=request.user, title="Test Categories", categories=[activity_categories])
+        activity = create_activity(owner=request.user, title="Test Categories",
+                                   categories=[activity_categories])
 
         searcher = BaseSearcher(request)
         query = searcher.get_index_query().first()
         self.assertEqual(query, activity)
 
     def test_place_search(self):
+        """
+        Test searching by place.
+
+        1. Create a request with 'tag' as 'place' and 'q' as 'Home'.
+        2. Set the user attribute for the request.
+        3. Create an activity with the place set to 'Home'.
+        4. Create a BaseSearcher instance for the request.
+        5. Get the first query result and assert it is the created activity.
+        """
         request = self.factory.get(reverse('action:index'), {'tag': 'place', 'q': 'Home'})
         request.user = self.user
         activity_home = create_activity(owner=request.user, title="Test Place", place="Home")
@@ -66,28 +124,59 @@ class SearchUtilsTests(TestCase):
         self.assertEqual(query, activity_home)
 
     def test_exact_date_search(self):
+        """
+        Test searching by exact date.
+
+        1. Get the string representation of yesterday's date.
+        2. Create a request with 'tag' as 'date_exact' and 'q' as the string representation of yesterday's date.
+        3. Set the user attribute for the request.
+        4. Create an activity with the start date set to yesterday.
+        5. Create a BaseSearcher instance for the request.
+        6. Get the first query result and assert it is the created activity.
+        """
         yesterday_string = (timezone.now() - timezone.timedelta(days=1)).strftime('%Y-%m-%dT%H:%M')
 
-        request = self.factory.get(reverse('action:index'), {'tag': 'date_exact', 'q': yesterday_string})
+        request = self.factory.get(reverse('action:index'),
+                                   {'tag': 'date_exact', 'q': yesterday_string})
         request.user = self.user
         activity_exact_date = create_activity(owner=request.user, title="Test Exact Match",
-                                        start_date=timezone.now() - timezone.timedelta(days=1))
+                                              start_date=timezone.now() - timezone.timedelta(
+                                                  days=1))
 
         searcher = BaseSearcher(request)
         query = searcher.get_index_query().first()
         self.assertEqual(query, activity_exact_date)
 
     def test_upcoming_search(self):
+        """
+        Test searching for upcoming activities.
+
+        1. Create a request with 'tag' as 'upcoming'.
+        2. Set the user attribute for the request.
+        3. Create an activity with a start date in the future.
+        4. Create a BaseSearcher instance for the request.
+        5. Get the first query result and assert it is the created activity.
+        """
         request = self.factory.get(reverse('action:index'), {'tag': 'upcoming'})
         request.user = self.user
         upcoming_activity = create_activity(
-            owner=request.user, title="Test Upcoming", pub_date=timezone.now() + timezone.timedelta(days=1))
+            owner=request.user, title="Test Upcoming",
+            pub_date=timezone.now() + timezone.timedelta(days=1))
 
         searcher = BaseSearcher(request)
         query = searcher.get_index_query().first()
         self.assertEqual(query, upcoming_activity)
 
     def test_popular_search(self):
+        """
+        Test searching for popular activities.
+
+        1. Create a request with 'tag' as 'popular'.
+        2. Set the user attribute for the request.
+        3. Create three activities with varying numbers of quick joins.
+        4. Create a BaseSearcher instance for the request.
+        5. Get the list of query results and assert it is in the expected order.
+        """
         request = self.factory.get(reverse('action:index'), {'tag': 'popular'})
         request.user = self.user
 
@@ -107,17 +196,37 @@ class SearchUtilsTests(TestCase):
         self.assertListEqual(query_list, [activity1, activity2, activity3])
 
     def test_recent_search(self):
+        """
+        Test searching for recent activities.
+
+        1. Create a request with 'tag' as 'recent'.
+        2. Set the user attribute for the request.
+        3. Create an activity with a start date in the past.
+        4. Create a BaseSearcher instance for the request.
+        5. Get the first query result and assert it is the created activity.
+        """
         request = self.factory.get(reverse('action:index'), {'tag': 'recent'})
         request.user = self.user
         recent_activity = create_activity(
-            owner=request.user, title="Test Recent", 
+            owner=request.user, title="Test Recent",
             pub_date=timezone.now() - timezone.timedelta(seconds=10000))
-        
+
         searcher = BaseSearcher(request)
         query = searcher.get_index_query().first()
         self.assertEqual(query, recent_activity)
 
     def test_registered_search(self):
+        """
+        Test searching for registered activities.
+
+        1. Create a request with 'tag' as 'registered'.
+        2. Set the user attribute for the request.
+        3. Create user and activity.
+        4. Create a BaseSearcher instance for the request.
+        5. Get the first query result and assert it is None since the user is not registered.
+        6. Create an activity status for the user and activity.
+        7. Get the first query result and assert it is the created activity.
+        """
         request = self.factory.get(reverse('action:index'), {'tag': 'registered'})
         request.user = self.user
         creator = create_user(username="Creator")
@@ -134,6 +243,17 @@ class SearchUtilsTests(TestCase):
         self.assertEqual(query, activity)
 
     def test_favorited_search(self):
+        """
+        Test searching for favorited activities.
+
+        1. Create a request with 'tag' as 'favorited'.
+        2. Set the user attribute for the request.
+        3. Create user and activity.
+        4. Create a BaseSearcher instance for the request.
+        5. Get the first query result and assert it is None since the activity is not favorited.
+        6. Create an activity status for the user and activity with favorited set to True.
+        7. Get the first query result and assert it is the created activity.
+        """
         request = self.factory.get(reverse('action:index'), {'tag': 'favorited'})
         request.user = self.user
         creator = create_user(username="Creator")
@@ -150,6 +270,18 @@ class SearchUtilsTests(TestCase):
         self.assertEqual(query, activity)
 
     def test_friends_search(self):
+        """
+        Test searching for activities joined by friends.
+
+        1. Create a request with 'tag' as 'friend_joined'.
+        2. Set the user attribute for the request.
+        3. Create friend user and activity.
+        4. Create a friend status with 'Accepted' status between the user and friend.
+        5. Create a BaseSearcher instance for the request.
+        6. Get the first query result and assert it is None since the friend has not joined.
+        7. Create an activity status for the friend and activity.
+        8. Get the first query result and assert it is the created activity.
+        """
         request = self.factory.get(reverse('action:index'), {'tag': 'friend_joined'})
         request.user = self.user
         friend = create_user(username="Friend")
@@ -171,15 +303,25 @@ class SearchUtilsTests(TestCase):
         self.assertEqual(query, activity)
 
     def test_multiple_categories_search(self):
+        """
+        Test searching by multiple categories.
+
+        1. Create multiple activities with different sets of categories.
+        2. Test different cases where specific categories are included in the search query.
+        3. Use the BaseSearcher to get the index query and check if the result matches the expected activities.
+        """
         activity_categories = [create_category(f"Category{i}") for i in range(1, 11)]
 
         # activity1 has category 1-5, activity2 has category 3-7, activity2 has category 6-10
         activity1 = create_activity(owner=create_user("New User 1"),
-                        title="Test Multiple Categories 1", categories=activity_categories[0:5])
+                                    title="Test Multiple Categories 1",
+                                    categories=activity_categories[0:5])
         activity2 = create_activity(owner=create_user("New User 2"),
-                        title="Test Multiple Categories 2", categories=activity_categories[2:7])
+                                    title="Test Multiple Categories 2",
+                                    categories=activity_categories[2:7])
         activity3 = create_activity(owner=create_user("New User 3"),
-                title="Test Multiple Categories 3", categories=activity_categories[5:10])
+                                    title="Test Multiple Categories 3",
+                                    categories=activity_categories[5:10])
 
         # Case 1: Category 1-2 in activity1
         request = self.factory.get(reverse('action:index'), {
@@ -222,12 +364,21 @@ class SearchUtilsTests(TestCase):
         self.assertListEqual(query, [activity3])
 
     def test_start_date_range_search(self):
+        """
+        Test searching by start date range.
+
+        1. Create multiple activities with different start dates.
+        2. Test different cases where the start date range is included in the search query.
+        3. Use the BaseSearcher to get the index query and check if the result matches the expected activities.
+        """
         activity1 = create_activity(owner=create_user("New User 1"),
-                title="Test Last Week", start_date=timezone.now() - timezone.timedelta(days=7))
+                                    title="Test Last Week",
+                                    start_date=timezone.now() - timezone.timedelta(days=7))
         activity2 = create_activity(owner=create_user("New User 2"),
-                title="Test Today", start_date=timezone.now())
+                                    title="Test Today", start_date=timezone.now())
         activity3 = create_activity(owner=create_user("New User 3"),
-                title="Test Next Week", start_date=timezone.now() + timezone.timedelta(days=7))
+                                    title="Test Next Week",
+                                    start_date=timezone.now() + timezone.timedelta(days=7))
 
         yesterday_string = (timezone.now() - timezone.timedelta(days=1)).strftime('%Y-%m-%dT%H:%M')
         tomorrow_string = (timezone.now() + timezone.timedelta(days=1)).strftime('%Y-%m-%dT%H:%M')
@@ -266,13 +417,20 @@ class SearchUtilsTests(TestCase):
         self.assertListEqual(query, [activity2])
 
     def test_multiple_tags_search(self):
+        """
+        Test searching with multiple tags.
+
+        1. Create multiple activities with different titles, owners, places, and start dates.
+        2. Test a case where multiple tags and queries are included in the search query.
+        3. Use the BaseSearcher to get the index query and check if the result matches the expected activities.
+        """
         user = create_user("Test User 1")
         activity1 = create_activity(owner=user,
-                        title="Test Tag", place="Test Place",
-                        start_date=timezone.now() - timezone.timedelta(days=1))
+                                    title="Test Tag", place="Test Place",
+                                    start_date=timezone.now() - timezone.timedelta(days=1))
         activity2 = create_activity(owner=user,
-                        title="Test Tag", place="Test Place", 
-                        start_date=timezone.now() - timezone.timedelta(days=10))
+                                    title="Test Tag", place="Test Place",
+                                    start_date=timezone.now() - timezone.timedelta(days=10))
 
         last_week_string = (timezone.now() - timezone.timedelta(days=7)).strftime('%Y-%m-%dT%H:%M')
         request = self.factory.get(reverse('action:index'), {
