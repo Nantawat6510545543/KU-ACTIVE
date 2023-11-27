decf6ad70e34ab7d2cbc2157ea083929c2f5a213
mossg <nantawat.suk@ku.th>
2023-11-27 18:14:09 +0700

    rename view

--- action/urls/profile_urls.py
+++ action/urls/profile_urls.py
@@ -4,6 +4,6 @@ from action import views
 
 profile_patterns = [
     path('', views.ProfileDetailView.as_view(), name='profile'),
-    path('edit', views.EditProfileView.as_view(), name='edit_profile'),
+    path('edit', views.ProfileEditView.as_view(), name='edit_profile'),
     path('<int:user_id>', views.ProfileDetailView.as_view(), name='profile'),
 ]
